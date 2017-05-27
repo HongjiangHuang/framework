459b559da22193afc47e5afe10d6223adbec9aca
zed <zedsmail@163.com>
2017-05-27 08:59:23 +0800

    搭建了测试框架,下次再完善

---
+++ tests/TestCase.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by zed.
+ */
+
+namespace Tests;
+
+use PHPUnit\Framework\TestCase as BaseTestCase;
+
+/**
+ * 所有测试都会集成这个类
+ * Class TestCase
+ * @package Tests
+ */
+class TestCase extends BaseTestCase
+{
+
+}
